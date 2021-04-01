274145a410b6b02faca48d09bfa92592d13a62c6
LucasLeandro1204 <lucasleandro1204@gmail.com>
2021-04-01 19:06:10 -0300

    chore: base style file and cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,12 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-</template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-})
+<script lang="ts" setup>
 </script>
 
-<style>
+<template>
+  akoefpko
+</template>
+
+<style lang="postcss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
 }
 </style>
