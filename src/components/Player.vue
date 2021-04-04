25ec8865437625b2369232d6a1df66f6075b6b44
LucasLeandro1204 <lucasleandro1204@gmail.com>
2021-04-04 15:18:14 -0300

    chore: instantiate videojs and update dependencies

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,9 +1,39 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import videojs from 'video.js';
+import { ref, watch } from '@vue/runtime-core';
+import 'video.js/dist/video-js.min.css';
+
+export default defineComponent({
+  setup () {
+    const video = ref<HTMLCanvasElement | null>();
+
+    watch(
+      video,
+      (element, _, onCleanup): void => {
+        const instance = videojs(element);
+
+        console.dir(instance)
+
+        onCleanup((): void => {
+
+        });
+      },
+      {
+        flush: 'post',
+      },
+    );
+  },
+});
+</script>
+
 <template>
   <div
     class="player"
   >
     <video
       src="../assets/big_buck_bunny.mp4"
+      ref="video"
       class="player__video"
     />
   </div>
