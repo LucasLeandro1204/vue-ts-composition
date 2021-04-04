65ac8f2813dfa7c6c74d93d42b0e8478745f0ae7
LucasLeandro1204 <lucasleandro1204@gmail.com>
2021-04-04 20:26:44 -0300

    chore: wip chat component

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -2,18 +2,38 @@
   <aside
     class="chat"
   >
-    <p>foo</p>
-    <p>bar</p>
+    <p
+      class="chat__message"
+    >
+      <span
+        class="chat__timestamp"
+      >
+        1:30m
+      </span>
+
+      some random message here
+    </p>
   </aside>
 </template>
 
 <style lang="postcss">
 .chat {
-  padding: 16px 24px;
   display: flex;
-  justify-content: flex-end;
+  overflow-y: auto;
+  padding: 16px 24px;
   flex-direction: column;
   color: var(--color-white);
   border: 1px solid var(--color-gray-darker);
+
+  &__message {
+    display: flex;
+    margin: 8px 0;
+    flex-direction: column;
+  }
+
+  &__timestamp {
+    opacity: .7;
+    font-size: 14px;
+  }
 }
 </style>
